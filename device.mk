bfe40040023a968644a13e0e675dcc08e6c59132
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    RMX3521: Use 6GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Saalim: We are keeping the default values in vendor properties.
    
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -9,6 +9,9 @@ $(call inherit-product, vendor/realme/RMX3521/RMX3521-vendor.mk)
 # Get virtual-ab-ota properties
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
