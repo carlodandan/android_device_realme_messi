faab6325df44bd5c573117a9ad1df6718cfab5bf
LuK1337 <priv.luk@gmail.com>
2021-01-06 19:30:19 +0100

    RMX3521: Build vndservicemanager
    * Needed for FCM >= R.
    
    Change-Id: I6d5eb8084127e295e4abec2c0192c0ad5b07b867

--- device.mk
+++ device.mk
@@ -411,6 +411,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
