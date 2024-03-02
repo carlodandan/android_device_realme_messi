ec8c39d467f7cb47ac203bbf719f88a9c7476743
Carlo Dandan <carlodandan.personal@proton.me>
2024-03-02 16:11:52 +0800

    messi: Imported WiFi configurations from stock. * Extracted from RMX3521_11.C.08_1080_202302141733 OTA.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- device.mk
+++ device.mk
@@ -160,3 +160,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
+
+# Wifi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
