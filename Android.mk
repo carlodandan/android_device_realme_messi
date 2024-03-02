14e5aa55f473c4286272e157ce3026d105ff2846
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-03-02 04:00:08 +0000

    messi: Make Android makefile search more recursive.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- Android.mk
+++ Android.mk
@@ -3,6 +3,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),messi)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifeq ($(TARGET_DEVICE),billie)
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
