49458ddd77b88f5686858708cf9c0108720bed13
Carlo Dandan <carlodandan.personal@proton.me>
2024-03-05 10:00:28 +0800

    messi: Fix device name.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),billie)
+ifeq ($(TARGET_DEVICE),messi)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
