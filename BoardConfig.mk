f7003b6db767aae1ac9cecd85a0729aeed854006
Carlo Dandan <carlodandan.personal@proton.me>
2024-03-15 23:18:27 +0800

    RMX3521: Fixed indent.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/hidl/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 	$(DEVICE_PATH)/hidl/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+	hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
 	vendor/lineage/config/device_framework_matrix.xml
 
 # Kernel
