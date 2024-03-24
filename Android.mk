398117642f919d74b1c9c361e57696a514742ddd
Carlo Dandan <carlodandan.personal@proton.me>
2024-03-24 17:20:56 +0800

    Revert "RMX3521: Remove oppo logical partitions when necessary."
    This reverts commit 20bd45fddfbe6ebc3d038afcf23d087f964d0e97.

--- Android.mk
+++ Android.mk
@@ -4,7 +4,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),RMX3521)
-  $(call add-radio-file,dynamic-remove-oppo)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
