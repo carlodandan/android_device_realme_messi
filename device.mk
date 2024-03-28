7be7d2675bae95a2f40bb37b70da4c14fc920959
Carlo Dandan <carlodandan.personal@proton.me>
2024-03-28 18:07:24 +0800

    RMX3521: Fixed path for media profiles.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- device.mk
+++ device.mk
@@ -200,7 +200,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/vendor/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
+    $(LOCAL_PATH)/configs/media/odm/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
